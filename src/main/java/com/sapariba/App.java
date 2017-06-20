494c8c008e7f4fee2b888231a2b4d558bc7d313e
prabhakargudi <prabhakar.gudi@sap.com>
2017-06-20 14:04:45 +0530

    MavenProject
    Testing

---
+++ src/main/java/com/sapariba/App.java
@@ -0,0 +1,13 @@
+package com.sapariba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
